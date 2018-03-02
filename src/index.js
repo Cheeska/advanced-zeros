fe142506b73394d63eb6c7462ed268e7623dcd65
Yana <YanaLSoltan@gmail.com>
2018-03-02 12:10:21 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    var p = [];
+    var a = [];
+    var sa = [];
+    var wholeBase = base;
+    var nextPrime = 2;
+    while (wholeBase > 1) {
+        while (wholeBase % nextPrime != 0) {
+            nextPrime++;
+        }
+        p.push(nextPrime);
+        sa.push(0);
+        var pow = 0;
+        while (wholeBase % nextPrime == 0) {
+            pow++;
+            wholeBase /= nextPrime;
+        }
+        a.push(pow);
+    }
+
+    for (var i = 2; i <= number; ++i) {
+        var tmp = i;
+        for (var j = 0; j < p.length; ++j) {
+            while (tmp % p[j] == 0) {
+                tmp /= p[j];
+                sa[j]++;
+            }
+        }
+    }
+
+    var zeroNumber = Math.floor(sa[0] / a[0]);
+    for (var i = 0; i < a.length; i++) {
+        zeroNumber = Math.min(zeroNumber, Math.floor(sa[i] / a[i]));
+    }
+
+    return zeroNumber;
+};

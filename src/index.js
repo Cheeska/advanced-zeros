09c6086de22311b3f46a5f9bab4d118f23aaaea2
Yana <YanaLSoltan@gmail.com>
2018-03-02 14:54:54 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 module.exports = function getZerosCount(number, base) {
+    var allPrimes = [
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107,
+        109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+        233, 239, 241, 251, 257
+    ];
     var p = [];
     var a = [];
     var sa = [];
     var wholeBase = base;
-    var nextPrime = 2;
+    var nextPrimeIndex = 0;
     while (wholeBase > 1) {
-        while (wholeBase % nextPrime != 0) {
-            nextPrime++;
+        while (wholeBase % allPrimes[nextPrimeIndex] != 0) {
+            nextPrimeIndex++;
         }
-        p.push(nextPrime);
+        p.push(allPrimes[nextPrimeIndex]);
         sa.push(0);
         var pow = 0;
-        while (wholeBase % nextPrime == 0) {
+        while (wholeBase % allPrimes[nextPrimeIndex] == 0) {
             pow++;
-            wholeBase /= nextPrime;
+            wholeBase /= allPrimes[nextPrimeIndex];
         }
         a.push(pow);
     }
